3d5c25efd54d7024b4212df1e9e42e84aae28ff1
Artyom K <artyomkarpov@gmail.com>
2023-05-23 10:11:21 +0300

    Empty file

---
+++ chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -0,0 +1,30 @@
+#%%
+import os
+import sys
+import numpy as np
+import einops
+from typing import Union, Optional, Tuple
+import torch as t
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+from jaxtyping import Float
+import functools
+from pathlib import Path
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
+from tqdm.notebook import tqdm
+
+# Make sure exercises are in the path
+chapter = r"chapter0_fundamentals"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part2_cnns"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+from plotly_utils import imshow, line, bar
+from part2_cnns.utils import display_array_as_img, display_soln_array_as_img
+import part2_cnns.tests as tests
+
+MAIN = __name__ == "__main__"
+
+#%%
