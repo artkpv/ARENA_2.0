61cd15904a36348fcdfa661e521e1c48396f6048
Artyom K <artyomkarpov@gmail.com>
2023-05-29 21:06:22 +0300

    save

--- chapter1_transformers/exercises/part1_transformer_from_scratch/answers.py
+++ chapter1_transformers/exercises/part1_transformer_from_scratch/answers.py
@@ -131,4 +131,211 @@ if MAIN:
         next_token = logits[0, -1].argmax(dim=-1)
         # Decode and print the result
         next_char = reference_gpt2.to_string(next_token)
+
+    pp(reference_gpt2.to_str_tokens(tokens))
 # %%
+batch = 1
+position = 35
+d_model = 768
+n_heads = 12
+n_layers = 12
+d_mlp = 3072  # (4 * d_model)
+d_head = 64 # (d_model / n_heads)
+
+if MAIN:
+    for activation_name, activation in cache.items():
+        # Only print for first layer
+        if ".0." in activation_name or "blocks" not in activation_name:
+            print(f"{activation_name:30} {tuple(activation.shape)}")
+
+#%%
+if MAIN:
+    for name, param in reference_gpt2.named_parameters():
+        # Only print for first layer
+        if ".0." in name or "blocks" not in name:
+            print(f"{name:18} {tuple(param.shape)}")
+
+# %%
+# As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
+
+if MAIN:
+    print(reference_gpt2.cfg)
+
+# %%
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+
+if MAIN:
+    cfg = Config()
+    print(cfg)
+
+# %%
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    print("Input shape:", input.shape)
+    output = layer(input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape)
+    try: reference_output = gpt2_layer(input)
+    except: reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct\n")
+
+
+
+# %%
+
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        m = t.mean(residual, dim=-1, keepdim=True)
+        v = t.var(residual, dim=-1, unbiased=False, keepdim=True)
+        res = (residual - m) / (v + self.cfg.layer_norm_eps).sqrt() * self.w + self.b
+        return res
+
+if MAIN:
+    rand_float_test(LayerNorm, [2, 4, 768])
+    load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
+# %%
+
+class Embed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(t.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        if self.cfg.debug:
+            print(tokens.shape)
+            print(self.W_E.shape)
+        #return nn.functional.one_hot(tokens, num_classes=self.W_E.shape[0]).to(t.float) @ self.W_E
+        return self.W_E[tokens]
+
+
+if MAIN:
+    rand_int_test(Embed, [2, 4])
+    load_gpt2_test(Embed, reference_gpt2.embed, tokens)
+
+# %%
+class PosEmbed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(t.empty((cfg.n_ctx, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        if self.cfg.debug:
+            print(f"{tokens.shape=}")
+            print(f"{self.W_pos.shape=}")
+        return einops.repeat(self.W_pos[:tokens.shape[1]], 'n d -> b n d', b=tokens.shape[0])
+
+if MAIN:
+    rand_int_test(PosEmbed, [2, 4])
+    load_gpt2_test(PosEmbed, reference_gpt2.pos_embed, tokens)
+
+# import circuitsvis as cv
+# from IPython.display import display
+# 
+# 
+# if MAIN:
+#     html = cv.attention.attention_patterns(
+#         tokens=reference_gpt2.to_str_tokens(reference_text), 
+#         attention=cache["pattern", 0][0]
+#     )
+#     display(html)
+# 
+
+#%%
+class Attention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_K = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_V = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_O = nn.Parameter(t.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        self.b_Q = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_K = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_V = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_O = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_Q, std=self.cfg.init_range)
+        nn.init.normal_(self.W_K, std=self.cfg.init_range)
+        nn.init.normal_(self.W_V, std=self.cfg.init_range)
+        nn.init.normal_(self.W_O, std=self.cfg.init_range)
+        self.register_buffer("IGNORE", t.tensor(-1e5, dtype=t.float32, device=device))
+
+    def forward(
+        self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        n_heads, d_model, d_head = self.W_Q.shape
+        resid_broadcased = einops.repeat(normalized_resid_pre, 'b p d -> b p h d', h=n_heads)
+
+        # Step 1 
+        query = resid_broadcased @ self.W_Q + self.b_Q  # b p h d_h
+        key = resid_broadcased @ self.W_K + self.b_K  # b p h d_h
+        query = einops.rearrange(query, 'b p h d_h -> b h p d_h')
+        key = einops.rearrange(key, 'b p h d_h -> b h d_h p')
+        attn_scores =  query @ key
+        attn_scores /= t.sqrt(d_head)
+        attn_scores =  self.apply_causal_mask(attn_scores)
+        attn_scores.softmax(dim=(-1))
+
+        # Step 2 
+        value = resid_broadcased @ self.W_V + self.b_V  # b p h d_h
+        # TODO
+
+
+    def apply_causal_mask(
+        self, attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"]
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        '''
+        Applies a causal mask to attention scores, and returns masked scores.
+        '''
+        mask = t.triu(t.ones_like(attn_scores),diagonal=0).bool()
+        return attn_scores.masked_fill_(mask, self.IGNORE)
+
+
+if MAIN:
+    rand_float_test(Attention, [2, 4, 768])
+    load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["normalized", 0, "ln1"])
