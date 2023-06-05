60b1b19ef74c2dc34766cd02371e855df1ddf12c
Artyom K <artyomkarpov@gmail.com>
2023-06-05 19:29:45 +0300

    save

--- chapter1_transformers/exercises/part6_othellogpt/answers.py
+++ chapter1_transformers/exercises/part6_othellogpt/answers.py
@@ -54,4 +54,171 @@ device = t.device("cuda" if t.cuda.is_available() else "cpu")
 t.set_grad_enabled(False)
 
 MAIN = __name__ == "__main__"
+
+from pprint import pp
+# %%
+cfg = HookedTransformerConfig(
+    n_layers = 8,
+    d_model = 512,
+    d_head = 64,
+    n_heads = 8,
+    d_mlp = 2048,
+    d_vocab = 61,
+    n_ctx = 59,
+    act_fn="gelu",
+    normalization_type="LNPre",
+    device=device,
+)
+model = HookedTransformer(cfg)
+# %%
+sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "synthetic_model.pth")
+# champion_ship_sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "championship_model.pth")
+model.load_state_dict(sd)
+# %%
+# An example input
+sample_input = t.tensor([[
+    20, 19, 18, 10,  2,  1, 27,  3, 41, 42, 34, 12,  4, 40, 11, 29, 43, 13, 48, 56, 
+    33, 39, 22, 44, 24,  5, 46,  6, 32, 36, 51, 58, 52, 60, 21, 53, 26, 31, 37,  9,
+    25, 38, 23, 50, 45, 17, 47, 28, 35, 30, 54, 16, 59, 49, 57, 14, 15, 55, 7
+]]).to(device)
+
+# The argmax of the output (ie the most likely next move from each position)
+sample_output = t.tensor([[
+    21, 41, 40, 34, 40, 41,  3, 11, 21, 43, 40, 21, 28, 50, 33, 50, 33,  5, 33,  5,
+    52, 46, 14, 46, 14, 47, 38, 57, 36, 50, 38, 15, 28, 26, 28, 59, 50, 28, 14, 28, 
+    28, 28, 28, 45, 28, 35, 15, 14, 30, 59, 49, 59, 15, 15, 14, 15,  8,  7,  8
+]]).to(device)
+
+assert (model(sample_input).argmax(dim=-1) == sample_output.to(device)).all()
+
+# %%
+os.chdir(section_dir)
+
+OTHELLO_ROOT = (section_dir / "othello_world").resolve()
+OTHELLO_MECHINT_ROOT = (OTHELLO_ROOT / "mechanistic_interpretability").resolve()
+
+if not OTHELLO_ROOT.exists():
+    !git clone https://github.com/likenneth/othello_world
+
+sys.path.append(str(OTHELLO_MECHINT_ROOT))
+
+# %%
+from mech_interp_othello_utils import plot_board, plot_single_board, plot_board_log_probs, to_string, to_int, int_to_label, string_to_label, OthelloBoardState
+# %%
+# Load board data as ints (i.e. 0 to 60)
+board_seqs_int = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_int_small.npy"), dtype=t.long)
+# Load board data as "strings" (i.e. 0 to 63 with middle squares skipped out)
+board_seqs_string = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_string_small.npy"), dtype=t.long)
+
+assert all([middle_sq not in board_seqs_string for middle_sq in [27, 28, 35, 36]])
+'''
+0 1 2 3  4 5 6 7
+8 9 0 1  2 3 4 15
+6 7 8 9  0 1 2 23
+4 5 6 7  8 9 0 31 
+
+2 3 4 5  6 7 8 39 
+0 1 2 3  4 5 6 47
+8 9 0 1  2 3 4 55
+6 7 8 9  0 1 2 63
+'''
+assert board_seqs_int.max() == 60
+
+num_games, length_of_game = board_seqs_int.shape
+print("Number of games:", num_games)
+print("Length of game:", length_of_game)
+
+# %%
+# Define possible indices (excluding the four center squares)
+stoi_indices = [i for i in range(64) if i not in [27, 28, 35, 36]]
+
+# Define our rows, and the function that converts an index into a (row, column) label, e.g. `E2`
+alpha = "ABCDEFGH"
+
+def to_board_label(i):
+    return f"{alpha[i//8]}{i%8}"
+
+# Get our list of board labels
+
+board_labels = list(map(to_board_label, stoi_indices))
+full_board_labels = list(map(to_board_label, range(64)))
+pp(board_labels)
+pp(full_board_labels)
+# %%
+moves_int = board_seqs_int[0, :30]
+
+# This is implicitly converted to a batch of size 1
+logits: Tensor = model(moves_int)
+print("logits:", logits.shape)
+# %%
+logit_vec = logits[0, -1]
+log_probs = logit_vec.log_softmax(-1)
+# Remove the "pass" move (the zeroth vocab item)
+log_probs = log_probs[1:]
+assert len(log_probs)==60
+
+# Set all cells to -13 by default, for a very negative log prob - this means the middle cells don't show up as mattering
+temp_board_state = t.zeros((8, 8), dtype=t.float32, device=device) - 13.
+temp_board_state.flatten()[stoi_indices] = log_probs
+# %%
+def plot_square_as_board(state, diverging_scale=True, **kwargs):
+    '''Takes a square input (8 by 8) and plot it as a board. Can do a stack of boards via facet_col=0'''
+    kwargs = {
+        "y": [i for i in alpha],
+        "x": [str(i) for i in range(8)],
+        "color_continuous_scale": "RdBu" if diverging_scale else "Blues",
+        "color_continuous_midpoint": 0. if diverging_scale else None,
+        "aspect": "equal",
+        **kwargs
+    }
+    imshow(state, **kwargs)
+
+
+plot_square_as_board(temp_board_state.reshape(8, 8), zmax=0, diverging_scale=False, title="Example Log Probs")
+# %%
+plot_single_board(int_to_label(moves_int))
+# %%
+num_games = 50
+focus_games_int = board_seqs_int[:num_games]
+focus_games_string = board_seqs_string[:num_games]
+def one_hot(list_of_ints, num_classes=64):
+    out = t.zeros((num_classes,), dtype=t.float32)
+    out[list_of_ints] = 1.
+    return out
+
+
+focus_states = np.zeros((num_games, 60, 8, 8), dtype=np.float32)
+focus_valid_moves = t.zeros((num_games, 60, 64), dtype=t.float32)
+
+for i in (range(num_games)):
+    board = OthelloBoardState()
+    for j in range(60):
+        board.umpire(focus_games_string[i, j].item())
+        focus_states[i, j] = board.state
+        focus_valid_moves[i, j] = one_hot(board.get_valid_moves())
+
+print("focus states:", focus_states.shape)
+print("focus_valid_moves", tuple(focus_valid_moves.shape))
+# %%
+imshow(
+    focus_states[0, :17],
+    facet_col=0,
+    facet_col_wrap=8,
+    facet_labels=[f"Move {i}" for i in range(1, 17)],
+    title="First 16 moves of first game",
+    color_continuous_scale="Greys",
+)
+# %%
+focus_logits, focus_cache = model.run_with_cache(focus_games_int[:, :-1].to(device))
+# %%
+print(focus_logits.shape)
+print(focus_cache['pattern', 7].shape)
+# %%
+full_linear_probe = t.load(OTHELLO_MECHINT_ROOT / "main_linear_probe.pth")
+
+rows = 8
+cols = 8 
+options = 3
+assert full_linear_probe.shape == (3, cfg.d_model, rows, cols, options)
+
 # %%
