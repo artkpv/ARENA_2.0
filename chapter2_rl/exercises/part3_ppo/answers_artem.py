b5c914f841681baab1abbb4f758dccc5d605a795
Artyom K <artyomkarpov@gmail.com>
2023-06-15 16:07:26 +0300

    save

--- chapter2_rl/exercises/part3_ppo/answers_artem.py
+++ chapter2_rl/exercises/part3_ppo/answers_artem.py
@@ -287,3 +287,268 @@ obs, dones, actions, logprobs, values, rewards = [t.stack(arr).to(device) for ar
 
 plot_cartpole_obs_and_dones(obs, dones, show_env_jumps=True)
 # %%
+minibatches = rb.get_minibatches(next_value, next_done)
+
+obs = minibatches[0].obs
+dones = minibatches[0].dones
+
+plot_cartpole_obs_and_dones(obs, dones)
+# %%
+class PPOAgent(nn.Module):
+    critic: nn.Sequential
+    actor: nn.Sequential
+
+    def __init__(self, args: PPOArgs, envs: gym.vector.SyncVectorEnv):
+        super().__init__()
+        self.args = args
+        self.envs = envs
+        self.num_envs = envs.num_envs
+        self.obs_shape = envs.single_observation_space.shape
+        self.num_obs = np.array(self.obs_shape).prod()
+        self.num_actions = envs.single_action_space.n
+
+        # Keep track of global number of steps taken by agent
+        self.steps = 0
+        # Define actor and critic (using our previous methods)
+        self.actor, self.critic = get_actor_and_critic(envs)
+
+        # Define our first (obs, done, value), so we can start adding experiences to our replay buffer
+        self.next_obs = t.tensor(self.envs.reset()).to(device)
+        self.next_done = t.zeros(self.envs.num_envs).to(device, dtype=t.float)
+
+        # Create our replay buffer
+        self.rb = ReplayBuffer(args, envs)
+
+    def play_step(self) -> List[dict]:
+        '''
+        Carries out a single interaction step between the agent and the environment, and adds results to the replay buffer.
+        '''
+        obs = self.next_obs
+        with t.inference_mode():
+            actions_logits = self.actor(obs)
+        distribution = Categorical(logits=actions_logits)
+        actions = distribution.sample()
+        log_probs = distribution.log_prob(actions)
+        next_obs, rewards, dones, infos = self.envs.step(actions.cpu().numpy())
+        next_obs = t.from_numpy(next_obs).to(device)
+        rewards = t.from_numpy(rewards).to(device)
+        dones = t.from_numpy(dones).to(device, dtype=t.float)
+        with t.inference_mode():
+            values = self.critic(obs).flatten()
+        self.rb.add(
+            obs, 
+            actions, 
+            rewards, 
+            dones, 
+            log_probs, 
+            values
+        )
+        self.steps += self.num_envs
+        self.next_obs = next_obs
+        self.next_done = dones
+
+    def get_minibatches(self) -> None:
+        '''
+        Gets minibatches from the replay buffer.
+        '''
+        with t.inference_mode():
+            next_value = self.critic(self.next_obs).flatten()
+        return self.rb.get_minibatches(next_value, self.next_done)
+
+
+tests.test_ppo_agent(PPOAgent)
+# %% 2️⃣ Learning Phase
+
+
+# %%
+def calc_clipped_surrogate_objective(
+    probs: Categorical, 
+    mb_action: Int[Tensor, "minibatch_size"], 
+    mb_advantages: Float[Tensor, "minibatch_size"], 
+    mb_logprobs: Float[Tensor, "minibatch_size"], 
+    clip_coef: float, 
+    eps: float = 1e-8
+) -> Float[Tensor, ""]:
+    '''Return the clipped surrogate objective, suitable for maximisation with gradient ascent.
+
+    probs:
+        a distribution containing the actor's unnormalized logits of shape (minibatch_size, num_actions)
+    mb_action:
+        what actions actions were taken in the sampled minibatch
+    mb_advantages:
+        advantages calculated from the sampled minibatch
+    mb_logprobs:
+        logprobs of the actions taken in the sampled minibatch (according to the old policy)
+    clip_coef:
+        amount of clipping, denoted by epsilon in Eq 7.
+    eps:
+        used to add to std dev of mb_advantages when normalizing (to avoid dividing by zero)
+    '''
+    assert mb_action.shape == mb_advantages.shape == mb_logprobs.shape    
+    mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + eps)
+    ratio = (probs.log_prob(mb_action) - mb_logprobs).exp()
+    objective = t.min(ratio * mb_advantages, t.clip(ratio, 1 - clip_coef, 1 + clip_coef) * mb_advantages)
+    return objective.mean()
+    
+tests.test_calc_clipped_surrogate_objective(calc_clipped_surrogate_objective)
+
+# %%
+def calc_value_function_loss(
+    values: Float[Tensor, "minibatch_size"],
+    mb_returns: Float[Tensor, "minibatch_size"],
+    vf_coef: float
+) -> Float[Tensor, ""]:
+    '''Compute the value function portion of the loss function.
+
+    values:
+        the value function predictions for the sampled minibatch (using the updated critic network)
+    mb_returns:
+        the target for our updated critic network (computed as `advantages + values` from the old network)
+    vf_coef:
+        the coefficient for the value loss, which weights its contribution to the overall loss. Denoted by c_1 in the paper.
+    '''
+    assert values.shape == mb_returns.shape
+    return vf_coef * (values - mb_returns).pow(2).mean() * 0.5
+
+
+tests.test_calc_value_function_loss(calc_value_function_loss)
+
+# %%
+def calc_entropy_bonus(probs: Categorical, ent_coef: float):
+    '''Return the entropy bonus term, suitable for gradient ascent.
+
+    probs:
+        the probability distribution for the current policy
+    ent_coef: 
+        the coefficient for the entropy loss, which weights its contribution to the overall objective function. Denoted by c_2 in the paper.
+    '''
+    return ent_coef * probs.entropy().mean()
+
+
+tests.test_calc_entropy_bonus(calc_entropy_bonus)
+
+# %%
+class PPOScheduler:
+    def __init__(self, optimizer: Optimizer, initial_lr: float, end_lr: float, total_training_steps: int):
+        self.optimizer = optimizer
+        self.initial_lr = initial_lr
+        self.end_lr = end_lr
+        self.total_training_steps = total_training_steps
+        self.n_step_calls = 0
+
+    def step(self):
+        '''Implement linear learning rate decay so that after total_training_steps calls to step, the learning rate is end_lr.
+        '''
+        self.n_step_calls += 1
+        lr = self.initial_lr + (self.end_lr - self.initial_lr) * min(
+            1.0, (self.n_step_calls / self.total_training_steps)
+        )
+        for pg in self.optimizer.param_groups:
+            pg['lr'] = lr
+
+def make_optimizer(agent: PPOAgent, total_training_steps: int, initial_lr: float, end_lr: float) -> Tuple[optim.Adam, PPOScheduler]:
+    '''Return an appropriately configured Adam with its attached scheduler.'''
+    optimizer = optim.Adam(agent.parameters(), lr=initial_lr, eps=1e-5, maximize=True)
+    scheduler = PPOScheduler(optimizer, initial_lr, end_lr, total_training_steps)
+    return (optimizer, scheduler)
+
+
+tests.test_ppo_scheduler(PPOScheduler)
+# %% 3️⃣ Training Loop
+
+# %% 
+class PPOTrainer:
+
+    def __init__(self, args: PPOArgs):
+        super().__init__()
+        self.args = args
+        set_global_seeds(args.seed)
+        self.run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+        self.envs = gym.vector.SyncVectorEnv([
+            make_env(args.env_id, args.seed + i, i, args.capture_video, self.run_name) 
+            for i in range(args.num_envs)
+        ])
+        self.agent = PPOAgent(self.args, self.envs).to(device)
+        self.optimizer, self.scheduler = make_optimizer(self.agent, self.args.total_training_steps, self.args.learning_rate, 0.0)
+        if args.use_wandb:
+            wandb.init(project=args.wandb_project_name, entity=args.wandb_entity, name=args.exp_name)
+            if args.capture_video: wandb.gym.monitor()
+
+    def rollout_phase(self):
+        '''Should populate the replay buffer with new experiences.'''
+        for step in tqdm(range(args.num_steps)):
+            self.agent.play_step()  # TODO. It should return dict?
+            
+    def learning_phase(self) -> None:
+        '''Should get minibatches and iterate through them (performing an optimizer step at each one).'''
+        for mb in self.agent.get_minibatches():
+            objective = self._compute_ppo_objective(mb)
+            objective.backward()
+            nn.utils.clip_grad_norm(self.agent.parameters(), self.args.max_grad_norm)
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            self.scheduler.step()
+
+    def _compute_ppo_objective(self, mb : ReplayBufferSamples) -> Float[Tensor, ""]:
+        actions_logits = self.agent.actor(mb.obs)
+        distribution = Categorical(logits=actions_logits)
+        values = self.agent.critic(mb.obs).squeeze()
+        return (
+            calc_clipped_surrogate_objective(
+                distribution, 
+                mb.actions, 
+                mb.advantages, 
+                mb.logprobs, 
+                self.args.clip_coef
+                #eps=
+            ) 
+            - calc_value_function_loss(values, mb.returns, self.args.vf_coef)
+            + calc_entropy_bonus(distribution, self.args.ent_coef)
+        )
+
+
+# %%
+def test_probe(probe_idx: int):
+
+    # Define a set of arguments for our probe experiment
+    args = PPOArgs(
+        env_id=f"Probe{probe_idx}-v0",
+        exp_name=f"test-probe-{probe_idx}", 
+        total_timesteps=[10000, 10000, 10000, 30000, 50000][probe_idx-1],
+        learning_rate=0.001,
+        capture_video=False,
+        use_wandb=False,
+    )
+
+    # YOUR CODE HERE - create a PPOTrainer instance, and train your agent
+    trainer = PPOTrainer(args)
+    agent = trainer.agent
+    # initialise PPOArgs and PPOTrainer objects
+    for epoch in range(args.total_epochs):
+        trainer.rollout_phase()
+        trainer.learning_phase()
+
+    # Check that our final results were the ones we expected from this probe
+    obs_for_probes = [[[0.0]], [[-1.0], [+1.0]], [[0.0], [1.0]], [[0.0]], [[0.0], [1.0]]]
+    expected_value_for_probes = [[[1.0]], [[-1.0], [+1.0]], [[args.gamma], [1.0]], [[1.0]], [[1.0], [1.0]]]
+    expected_probs_for_probes = [None, None, None, [[0.0, 1.0]], [[1.0, 0.0], [0.0, 1.0]]]
+    tolerances = [5e-4, 5e-4, 5e-4, 1e-3, 1e-3]
+    obs = t.tensor(obs_for_probes[probe_idx-1]).to(device)
+    with t.inference_mode():
+        value = agent.critic(obs)
+        probs = agent.actor(obs).softmax(-1)
+    expected_value = t.tensor(expected_value_for_probes[probe_idx-1]).to(device)
+    t.testing.assert_close(value, expected_value, atol=tolerances[probe_idx-1], rtol=0)
+    expected_probs = expected_probs_for_probes[probe_idx-1]
+    if expected_probs is not None:
+        t.testing.assert_close(probs, t.tensor(expected_probs).to(device), atol=tolerances[probe_idx-1], rtol=0)
+    clear_output()
+    print("Probe tests passed!")
+
+
+test_probe(1)
+test_probe(2)
+test_probe(4)
+test_probe(5)
+test_probe(3)
+# %%
