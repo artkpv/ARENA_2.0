402fd192f44b7f47c4d9c036b5b083c7f28d5b6d
ARENA Account <pranav@arena.education>
2023-06-20 09:14:52 +0000

    save

--- chapter3_training_at_scale/exercises/part1_gpus/answers_artem.py
+++ chapter3_training_at_scale/exercises/part1_gpus/answers_artem.py
@@ -31,7 +31,8 @@ import part1_gpus.tests as tests
 root_dir = exercises_dir.parent.parent.resolve()
 if str(root_dir) not in sys.path: sys.path.append(str(root_dir))
 os.chdir(root_dir)
-from chapter0_fundamentals.exercises.part3_resnets.solutions import ResNet34
+#from chapter0_fundamentals.exercises.part3_resnets.solutions import ResNet34
+from chapter0_fundamentals.exercises.part3_resnets.answers import ResNet34
 os.chdir(orig_dir)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -104,4 +105,160 @@ with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
 output = prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=10)
 print(output)
 prof.export_chrome_trace("trace.json")
+
+# %%
+model = ResNet34()
+inputs = torch.randn(5, 3, 224, 224)
+
+with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    with record_function("my_model_in_inference"):
+        with torch.inference_mode():
+            model(inputs)
+
+print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+# %%
+model = ResNet34()
+inputs = torch.randn(5, 3, 224, 224)
+
+with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    with record_function("my_model_in_no_grad"):
+        with torch.no_grad():
+            model(inputs)
+
+print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+# %%
+model = ResNet34().cuda()
+inputs = torch.randn(5, 3, 224, 224).cuda()
+
+with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
+    model(inputs)
+
+prof.export_chrome_trace("trace.json")
+
+output = prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=10)
+print(output)
+# %% 
+# 2️⃣ Kernel Fusion and Benchmarking
+
+# %%
+def daxpy(alpha,x,y):
+  return alpha*x + y
+
+@torch.jit.script
+def fused_daxpy(alpha,x,y):
+    return alpha*x + y
+
+def test_daxpy_random_input(fn1, fn2):
+
+    #alpha = torch.rand(1, device='cuda')
+    alpha = torch.randint(1, 100, size=(1,), device='cuda')
+    #x = torch.randn(1823*10, 1823, device='cuda')
+    x = torch.randint(1, 1_000_000, size=(1823, 1823), device='cuda')
+    #y = torch.randn(1823*10, 1823, device='cuda')
+    y = torch.randint(1, 1_000_000, size=(1823, 1823), device='cuda')
+
+    assert torch.allclose(fn1(alpha, x, y), fn2(alpha, x, y), 0, 1e-6), "Implementations are not analogous"
+    print('Tests passed')
+
+test_daxpy_random_input(daxpy, fused_daxpy)
+
+print("benching...")
+bench_results = []
+for contender in [daxpy, fused_daxpy]:
+    try:
+        name = contender.__name__
+    except:
+        name = contender.name
+
+    t = benchmark.Timer(
+        setup="alpha, x, y = torch.rand(1, device='cuda'),torch.randn(1823, 1823, device='cuda'), torch.randn(1823, 1823, device='cuda') ",
+        stmt="function(alpha, x, y)",
+        description=f"cuda",
+        label="daxpy",
+        sub_label=name,
+        globals={
+            'function': contender
+        }
+      )
+    bench_results.append(t.blocked_autorange(min_run_time=5))
+
+
+compare = benchmark.Compare(bench_results)
+compare.colorize()
+compare.print()
+# %%
+def naive_softmax(x):
+    '''Compute row-wise softmax of X using native pytorch
+
+    We subtract the maximum element in order to avoid overflows. Softmax is invariant to
+    this shift.
+    '''
+    # read  MN elements ; write M  elements
+    x_max = x.max(dim=1)[0]
+    # read MN + M elements ; write MN elements
+    z = x - x_max[:, None]
+    # read  MN elements ; write MN elements
+    numerator = torch.exp(z)
+    # read  MN elements ; write M  elements
+    denominator = numerator.sum(dim=1)
+    # read MN + M elements ; write MN elements
+    ret = numerator / denominator[:, None]
+    # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
+    return ret
+
+@torch.jit.script
+def fused_naive_softmax(x):
+    '''Compute row-wise softmax of X using native pytorch
+
+    We subtract the maximum element in order to avoid overflows. Softmax is invariant to
+    this shift.
+    '''
+    # read  MN elements ; write M  elements
+    x_max = x.max(dim=1)[0]
+    # read MN + M elements ; write MN elements
+    z = x - x_max[:, None]
+    # read  MN elements ; write MN elements
+    numerator = torch.exp(z)
+    # read  MN elements ; write M  elements
+    denominator = numerator.sum(dim=1)
+    # read MN + M elements ; write MN elements
+    ret = numerator / denominator[:, None]
+    # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
+    return ret
+
+
+def test_daxpy_random_input(fn1, fn2):
+    x = torch.randn(1823*10, 1823, device='cuda')
+    assert torch.allclose(fn1(x), fn2(x), 0, 1e-6), "Implementations are not analogous"
+    print('Tests passed')
+
+test_daxpy_random_input(naive_softmax, fused_naive_softmax)
+
+print("benching...")
+bench_results = []
+for contender in [naive_softmax, fused_naive_softmax]:
+    try:
+        name = contender.__name__
+    except:
+        name = contender.name
+
+    t = benchmark.Timer(
+        setup="x = torch.randn(1823, 1823, device='cuda')",
+        stmt="function(x)",
+        description=f"cuda",
+        label="naive_softmax",
+        sub_label=name,
+        globals={
+            'function': contender
+        }
+      )
+    bench_results.append(t.blocked_autorange(min_run_time=5))
+
+
+compare = benchmark.Compare(bench_results)
+compare.colorize()
+compare.print()
+# %%
+# 3️⃣ Quantization
+
 # %%
