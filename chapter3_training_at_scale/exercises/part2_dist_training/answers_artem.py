57e6c9322400fce07e0102836abee6913633f22e
ARENA Account <pranav@arena.education>
2023-06-20 17:49:30 +0000

    save

--- chapter3_training_at_scale/exercises/part2_dist_training/answers_artem.py
+++ chapter3_training_at_scale/exercises/part2_dist_training/answers_artem.py
@@ -21,3 +21,38 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 MAIN = __name__ == "__main__"
 # %%
+from threading import Thread
+from time import sleep
+
+# Add to the global variable
+def adder(amount, repeats):
+    global value
+    for _ in range(repeats):
+        value += amount
+
+# Subtract from the global variable
+def subtractor(amount, repeats):
+    global value
+    for _ in range(repeats):
+        value -= amount
+
+def add_and_subtract():
+    # Start a thread making additions
+    adder_thread = Thread(target=adder, args=(1, 10))
+    adder_thread.start()
+    #sleep(0.000001) 
+    # Start a thread making subtractions
+    subtractor_thread = Thread(target=subtractor, args=(1, 1000000))
+    subtractor_thread.start()
+    # Wait for both threads to finish
+    print('Waiting for threads to finish...')
+    adder_thread.join()
+    subtractor_thread.join()
+    # Print the value
+    print(f'Value: {value}')
+
+
+if __name__ == '__main__':
+    value = 0
+    add_and_subtract()
+# %%
