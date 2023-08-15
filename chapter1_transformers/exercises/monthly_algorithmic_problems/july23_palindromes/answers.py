a37578ecadbc431f6bad20dd870d989396aa59dd
Artyom K <Artyom K>
2023-08-15 11:25:21 +0300

    save

--- chapter1_transformers/exercises/monthly_algorithmic_problems/july23_palindromes/answers.py
+++ chapter1_transformers/exercises/monthly_algorithmic_problems/july23_palindromes/answers.py
@@ -36,3 +36,25 @@ state_dict = model.center_unembed(state_dict)
 state_dict = model.fold_layer_norm(state_dict)
 state_dict = model.fold_value_biases(state_dict)
 model.load_state_dict(state_dict, strict=False);
+
+# %%
+W_U_mean_over_input = einops.reduce(model.W_U, "d_model d_vocab -> d_model", "mean")
+t.testing.assert_close(W_U_mean_over_input, t.zeros_like(W_U_mean_over_input))
+
+W_U_mean_over_output = einops.reduce(model.W_U, "d_model d_vocab -> d_vocab", "mean")
+t.testing.assert_close(W_U_mean_over_output, t.zeros_like(W_U_mean_over_output))
+
+W_O_mean_over_output = einops.reduce(model.W_O, "layer head d_head d_model -> layer head d_head", "mean")
+t.testing.assert_close(W_O_mean_over_output, t.zeros_like(W_O_mean_over_output))
+
+b_O_mean_over_output = einops.reduce(model.b_O, "layer d_model -> layer", "mean")
+t.testing.assert_close(b_O_mean_over_output, t.zeros_like(b_O_mean_over_output))
+
+W_E_mean_over_output = einops.reduce(model.W_E, "token d_model -> token", "mean")
+t.testing.assert_close(W_E_mean_over_output, t.zeros_like(W_E_mean_over_output))
+
+W_pos_mean_over_output = einops.reduce(model.W_pos, "position d_model -> position", "mean")
+t.testing.assert_close(W_pos_mean_over_output, t.zeros_like(W_pos_mean_over_output))
+
+b_V = model.b_V
+t.testing.assert_close(b_V, t.zeros_like(b_V))
