90c097821c4b6997efa25bc07ecdf2faa90cbc36
Artyom K <Artyom K>
2023-08-15 11:24:33 +0300

    save

--- chapter1_transformers/exercises/monthly_algorithmic_problems/july23_palindromes/answers.py
+++ chapter1_transformers/exercises/monthly_algorithmic_problems/july23_palindromes/answers.py
@@ -16,3 +16,23 @@ from plotly_utils import hist, bar, imshow
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
 # %%
+filename = section_dir / "palindrome_classifier.pt"
+
+model = create_model(
+    half_length=10, # this is half the length of the palindrome sequences
+    max_value=30, # values in palindrome sequence are between 0 and max_value inclusive
+    seed=42,
+    d_model=28,
+    d_head=14,
+    n_heads=2,
+    normalization_type="LN",
+    d_mlp=None # this is an attn-only model
+)
+
+state_dict = t.load(filename)
+
+state_dict = model.center_writing_weights(t.load(filename))
+state_dict = model.center_unembed(state_dict)
+state_dict = model.fold_layer_norm(state_dict)
+state_dict = model.fold_value_biases(state_dict)
+model.load_state_dict(state_dict, strict=False);
